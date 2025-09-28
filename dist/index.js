33fd8b83dbf6acca4f5f0152392fa1f19a17ffb9
albi ariza <albyei24@gmail.com>
2025-09-28 21:27:36 +0700

    setting index js

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,47 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
-const catatanRoute_1 = __importDefault(require("./routers/catatanRoute"));
-const swagger_1 = require("./swagger"); // otomatis baca src/swagger.ts
-const global_1 = require("./global");
-const app = (0, express_1.default)();
-(0, swagger_1.setupSwagger)(app);
-app.use((0, cors_1.default)());
-app.use(`/catatan`, catatanRoute_1.default);
-app.listen(global_1.PORT, () => {
-    console.log(`[server]: Server is running at http://localhost:${global_1.PORT}`);
+// "use strict";
+// var __importDefault = (this && this.__importDefault) || function (mod) {
+//     return (mod && mod.__esModule) ? mod : { "default": mod };
+// };
+// Object.defineProperty(exports, "__esModule", { value: true });
+// const express_1 = __importDefault(require("express"));
+// const cors_1 = __importDefault(require("cors"));
+// const catatanRoute_1 = __importDefault(require("./routers/catatanRoute"));
+// const swagger_1 = require("./swagger"); // otomatis baca src/swagger.ts
+// const global_1 = require("./global");
+// const app = (0, express_1.default)();
+// (0, swagger_1.setupSwagger)(app);
+// app.use((0, cors_1.default)());
+// app.use(`/catatan`, catatanRoute_1.default);
+// app.listen(global_1.PORT, () => {
+//     console.log(`[server]: Server is running at http://localhost:${global_1.PORT}`);
+// });
+// //# sourceMappingURL=index.js.map
+
+import express from "express";
+import cors from "cors";
+import catatanRoute from "./routers/catatanRoute";
+import { setupSwagger } from "./swagger";
+import { PORT } from "./global";
+import path from "path";
+
+const app = express();
+
+// Setup EJS sebagai view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Middleware untuk file statis (CSS, JS, dll.)
+app.use(express.static(path.join(__dirname, "public")));
+
+// Middleware untuk parsing JSON dan URL-encoded (form data)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+app.use("/catatan", catatanRoute);
+
+setupSwagger(app);
+
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
-//# sourceMappingURL=index.js.map
--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-const http = require("http");
-console.log("Hello via Node.js!");
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Catatan Keuangan Server\n");
+import express from "express";
+import cors from "cors";
+import catatanRoute from "./routers/catatanRoute";
+import { setupSwagger } from "./swagger";
+import { PORT } from "./global";
+import path from "path";
+
+const app = express();
+
+// Setup EJS sebagai view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Middleware untuk file statis (CSS, JS, dll.)
+app.use(express.static(path.join(__dirname, "public")));
+
+// Middleware untuk parsing JSON dan URL-encoded (form data)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+app.use("/catatan", catatanRoute);
+
+setupSwagger(app);
+
+app.listen(PORT, () => {
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
 });
-server.listen(3000, () => console.log("Server running on port 3000"));
