ae64e750952e3975a558f8e74e9f8aeeb2c5c277
albi ariza <albyei24@gmail.com>
2025-09-28 18:33:11 +0700

    Index JS

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+const http = require("http");
+console.log("Hello via Node.js!");
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Catatan Keuangan Server\n");
+});
+server.listen(3000, () => console.log("Server running on port 3000"));
