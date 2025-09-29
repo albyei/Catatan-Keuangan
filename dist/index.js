2c8ba6652983795f2f752d1e0f9be8da98c09d10
albi ariza <albyei24@gmail.com>
2025-09-29 11:59:35 +0700

    setting index js

--- dist/index.js
+++ dist/index.js
@@ -25,6 +25,7 @@ import { PORT } from "./global";
 import path from "path";
 
 const app = express();
+const express_1 = require("express");
 
 // Setup EJS sebagai view engine
 app.set("view engine", "ejs");
@@ -44,4 +45,11 @@ setupSwagger(app);
 
 app.listen(PORT, () => {
   console.log(`[server]: Server is running at http://localhost:${PORT}`);
+
+  const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use("/catatan", catatanRoute_1.default);
+app.listen(global_1.PORT, () => {
+  console.log(`[server]: Server is running at http://localhost:${global_1.PORT}`);
+});
 });
--- src/swagger.ts
+++ src/swagger.ts
@@ -1,26 +1,26 @@
-// import swaggerJSDoc from "swagger-jsdoc";
-// import swaggerUi from "swagger-ui-express";
-// import { Express } from "express";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { Express } from "express";
 
-// const options: swaggerJSDoc.Options = {
-//   definition: {
-//     openapi: "3.0.0",
-//     info: {
-//       title: "API Documentation",
-//       version: "1.0.0",
-//       description: "Dokumentasi API untuk project backend",
-//     },
-//     servers: [
-//       {
-//         url: "http://localhost:3000", // ganti sesuai URL API
-//       },
-//     ],
-//   },
-//   apis: ["./src/routes/*.ts"], // lokasi file route Umak
-// };
+const options: swaggerJSDoc.Options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API Documentation",
+      version: "1.0.0",
+      description: "Dokumentasi API untuk project backend",
+    },
+    servers: [
+      {
+        url: "http://localhost:9000", // ganti sesuai URL API
+      },
+    ],
+  },
+  apis: ["./src/routes/*.ts"], // lokasi file route Umak
+};
 
-// const swaggerSpec = swaggerJSDoc(options);
+const swaggerSpec = swaggerJSDoc(options);
 
-// export function setupSwagger(app: Express) {
-//   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-// }
+export function setupSwagger(app: Express) {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+}
